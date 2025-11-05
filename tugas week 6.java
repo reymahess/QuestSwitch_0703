d29572f9881c014ec3eb3ddd191ddfaacdf9a418
reymahess <reynatamahessa>
2025-11-05 16:52:28 +0700

    merubah ke double

--- tugas week 6.java
+++ tugas week 6.java
@@ -4,7 +4,19 @@ public class tugasweek6 {
 
     public static void main(String[] args) {
         Scanner go = new Scanner(System.in);
-        int strukturData, pemograman, basisData, algoritma, rpl;
+        double strukturData, pemograman, basisData, algoritma, rpl, ipk;
         
+        System.out.println("Masukkan Nilai Struktur Data:");
+        strukturData = go.nextDouble();
+        System.out.println("Masukkan Nilai Pemograman :");
+        pemograman = go.nextDouble  ();
+        System.out.println("Masukkan Nilai Basis Data :");
+        basisData = go.nextDouble();
+        System.out.println("Masukkan Nilai Algoritma :");
+        algoritma = go.nextDouble();
+        System.out.println("Masukkan Nilai Rekayasa Perangkat Lunak :");
+        rpl = go.nextDouble();
+
+
     }
 }
